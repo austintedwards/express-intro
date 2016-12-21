12696946082fbdfbfab96376142f6417c7ac44dc
Austin Edwards <austintedwards@gmail.com>
2016-12-21 11:40:42 -0700

    API Creation

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.get('/',function(request, response){
 response.send("Hello World");
 });
 
+app.get('/api',(req,res)=>{
+let products =[
+  {name: "JavaScript Book", price:9.99},
+  {name: "A Christmas Carol", price: 19.99}
+];
+let stringified = JSON.stringify(products)
+res.send(stringified)
+});
+
 app.listen(3000, function(){
   console.log("Listen on port 3000. Go to http://locahost:3000/");
 });
