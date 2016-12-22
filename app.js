078c51f81ac0f389e8d75e34ae1becb0a0550ed0
Austin Edwards <austintedwards@gmail.com>
2016-12-22 14:41:16 -0700

    Part 10

--- app.js
+++ app.js
@@ -7,15 +7,33 @@ app.get('/',function(request, response){
 response.send("Hello World");
 });
 
-app.get('/api',(req,res)=>{
 let products =[
-  {name: "JavaScript Book", price:9.99},
-  {name: "A Christmas Carol", price: 19.99}
+  {id:1, name: "JavaScript Book", price:9.99},
+  {id:2, name: "A Christmas Carol", price: 19.99},
+  {id:3, name: "EggNOG", price: 2.99},
+  {id:4, name: "Breck Bourbon", price: 29.99},
+  {id:5, name: "A Christmas Story", price: 5.99}
+
+
 ];
+
+app.get('/api/products',(req,res)=>{
 let stringified = JSON.stringify(products)
 res.send(stringified)
 });
 
+app.get('/api/products/:id', (req,res)=>{
+  let id = Number(req.params.id)
+  let product = products.find((element)=> element.id ===id)
+  let stringified = JSON.stringify(product)
+  res.send(stringified)
+});
+
+const json_parson = bodyPar.json();
+app.post('/api/products', (req, res) =>{
+
+})
+
 app.listen(3000, function(){
-  console.log("Listen on port 3000. Go to http://locahost:3000/");
+  console.log("Listen on port 3000. Go to http://localhost:3000/");
 });
